4dd55b7e0426bafdc499a00a3b91ddd4b7291d2a
Genrevvv <genrevvaguilar@gmail.com>
2025-04-20 01:51:47 +0800

    Create prev and next button and functionality for book chapters

--- js/book.js
+++ js/book.js
@@ -13,28 +13,62 @@ const options = {
 fetch(`/load-book`, options)
     .then(res => res.json())
     .then(data => {        
-        console.log(data);
-        
-        // const bookSection = data['bookSection'];
+        // console.log(data);
         const bookName = data['bookName'];
-        const verses = data['verses'].filter(verse => verse.chapter === 1);
+        let chapterNum = 1;
+
 
         const title = document.getElementById('book-title');
         title.innerText = `${bookName}`;
 
-        const headerCont = document.getElementById('header');
-        headerCont.innerHTML = `<h1>${bookName}</h1>
-                                <h3>Chapter 1</h3>`;
+        let verses = data['verses'].filter(verse => verse.chapter === chapterNum);
+        loadChapter();
 
-        const versesCont = document.getElementById('verses');
+        const prevBtn = document.getElementById('prev');
+        const nextBtn = document.getElementById('next');
 
-        let verseElmnt = null;
-        for (verse of verses) {
-            verseElmnt = document.createElement('div');
-            verseElmnt.classList.add('verse');
-            verseElmnt.innerHTML = `<span>${verse['verse']}</span>
-                                    <p>${verse['text']}</p>`;
+        prevBtn.onclick = () => {
+            chapterNum--;
+            verses = data['verses'].filter(verse => verse.chapter === chapterNum);
 
-            versesCont.appendChild(verseElmnt);
+            if (verses.length === 0) {
+                chapterNum++;
+            }
+            else {
+                loadChapter();
+            }
+        };
+
+        nextBtn.onclick = () => {
+            chapterNum++;
+            verses = data['verses'].filter(verse => verse.chapter === chapterNum);
+
+            if (verses.length === 0) {
+                chapterNum--;
+            }
+            else {
+                loadChapter();
+            }        
+        };
+
+        function loadChapter() {
+            const headerCont = document.getElementById('header');
+            headerCont.innerHTML = '';
+            headerCont.innerHTML = `<h1>${bookName}</h1>
+                                    <h3>Chapter ${chapterNum}</h3>`;
+
+            const versesCont = document.getElementById('verses');
+            versesCont.innerHTML = '';
+
+            let verseElmnt = null;
+            for (verse of verses) {
+                verseElmnt = document.createElement('div');
+                verseElmnt.classList.add('verse');
+                verseElmnt.innerHTML = `<span>${verse['verse']}</span>
+                                        <p>${verse['text']}</p>`;
+    
+                versesCont.appendChild(verseElmnt);
+            }
         }
+        
 });
